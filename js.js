22ec36f8934dbf06bd6aed505851e238235f3c95
islamtes <yuldashevislam697@gmail.com>
2024-01-08 23:53:26 +0500

    js

---
+++ js.js
@@ -0,0 +1,49 @@
+// const arr = ['hayot', 'doston', 'asad'];
+//    const arr = ['hayot', 'doston', 'asad'];
+
+//    for(let res in arr){
+//     console.log(arr[res]);
+//    }
+
+// // console.log(arr[0]);
+
+// // arr.forEach((item) =>{
+// //     console.log(item+"bek");
+
+// // })
+
+//     for(let i = 0; i<arr.length; i++){
+
+
+// //     // console.log(arr)
+//     console.log(arr[i].charAt(0).toUpperCase()+arr[i].slice(1));
+//     }
+
+// for (let i = 0; i <= 30; i++) {
+
+//     if (i % 3 == 0) {
+//         console.log(`${i}  fiz`);
+//     } else if (i % 5 == 0) {
+//         console.log(`${i} buz`);
+//     }if (i % 15 == 0) {
+//         console.log(`${i} fiz buz`);
+//     }
+    
+// }
+
+let str = "salom"
+let count = '';
+
+for(let i =0; i<str.length; i++){
+    // console.log(str.length)
+
+
+    if(str[i] == "xx"){
+        
+        count ++
+    }
+    
+}
+
+
+
